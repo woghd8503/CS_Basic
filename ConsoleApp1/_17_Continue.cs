5373dceec2c128e6a2a2289b6a5a89d43e7bf0b7
DESKTOP-IAJOJ2N\user <woghd8503@naver.com>
2021-01-03 02:38:10 +0900

    //  continue 반복문 안에서 특정한 값을 걸러낼 때 사용

---
+++ ConsoleApp1/_17_Continue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Bitcamp
+{
+    class _17_Continue
+    {
+        static void Main()
+        {
+            int i = 0;
+            int sum = 0;
+            while (i < 100)
+            {
+                i++;
+                if (i % 2 == 0)
+                    continue;
+                sum = sum + 1;
+            }
+            Console.WriteLine("1 ~ 100중에 홀수들의 합 = {0}", sum);
+        }
+    }
+}
+
+//  continue 반복문 안에서 특정한 값을 걸러낼 때 사용 
