ac260f7b329f1136703effac1a71b1057dc15938
DESKTOP-IAJOJ2N\user <woghd8503@naver.com>
2021-01-03 12:57:41 +0900

    // C#의 배열은 System.Array 클래스를 상속받게 됩니다. // 그러므로 클래스라고 할 수 있습니다.

---
+++ ConsoleApp1/_23_ Array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Bitcamp
+{
+    class _23__Array
+    {
+        static void Main()
+        {
+            int[] ar;
+            ar = new int[5];
+            for(int a = 0; a < 5; a++)
+            {
+                ar[a] = a * 2;
+            }
+            for(int b = 0; b < 5; b++)
+            {
+                Console.WriteLine(ar[b]);
+            }
+        }
+    }
+}
+
+// C#의 배열은 System.Array 클래스를 상속받게 됩니다.
+// 그러므로 클래스라고 할 수 있습니다.
+// 사용법은 C, C++과 유사하지만 클래스이기 때문에 메서드를 가지고 있습니다.
+// 그래서 보다 편리하게 사용할 수 있습니다.
