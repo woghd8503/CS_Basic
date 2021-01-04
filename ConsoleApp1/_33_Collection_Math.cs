d5e0a09c8fdd58ce3d9899e09c8ef7697615204b
DESKTOP-IAJOJ2N\user <woghd8503@naver.com>
2021-01-04 23:32:21 +0900

    // Collection_Random 난수로 여러군데 * 출력

---
+++ ConsoleApp1/_33_Collection_Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Bitcamp
+{
+    class _33_Collection_Math
+    {
+        static void Main()
+        {
+            for(double angle = 0; angle < 360; angle += 10)
+            {
+                Console.CursorLeft = 40 + (int)(Math.Cos(angle * Math.PI / 180) * 20);
+                Console.CursorTop = 12 + (int)(Math.Sin(angle * Math.PI / 180) * 10);
+                Console.Write('.');
+            }
+        }
+    }
+}
+
+ㅊ
