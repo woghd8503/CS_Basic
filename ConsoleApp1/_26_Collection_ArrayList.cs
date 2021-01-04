8621c42ce0293feba83c67c10aeb30a186deaf53
DESKTOP-IAJOJ2N\user <woghd8503@naver.com>
2021-01-04 22:24:39 +0900

    // Collection_ArrayList

---
+++ ConsoleApp1/_26_Collection_ArrayList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections; // 추가해야 됨
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Bitcamp
+{
+    class _26_Collection_ArrayList
+    {
+        static void Main()
+        {
+            ArrayList ar = new ArrayList(10);
+            ar.Add(1);
+            ar.Add(2.34);
+            ar.Add("string");
+            ar.Add(new DateTime(2021, 1, 4));
+            ar.Insert(1, 1234);
+
+            foreach(object o in ar)
+            {
+                Console.WriteLine(o.ToString());
+            }
+            Console.WriteLine("개수 : " + ar.Count);
+            Console.WriteLine("용량 : " + ar.Capacity);
+        }
+    }
+}
