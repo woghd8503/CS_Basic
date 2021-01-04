240b34589facddb6c7036ba6fed2914db26ad029
DESKTOP-IAJOJ2N\user <woghd8503@naver.com>
2021-01-04 23:12:07 +0900

    //  Collection_Stopwatch compile 시간측정

---
+++ ConsoleApp1/_31_Collection_Stopwatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace CS_Bitcamp
+{
+    class _31_Collection_Stopwatch
+    {
+        static void Main()
+        {
+            Stopwatch st = new Stopwatch();
+            st.Start();
+            for(int i = 0; i < 100000; i++)
+            {
+                Console.Write(i + ",");
+            }
+            st.Stop();
+            Console.WriteLine("\n총 경과시간 = {0}", st.Elapsed);
+        }
+    }
+}
+
+//  Collection_Stopwatch compile 시간측정 
