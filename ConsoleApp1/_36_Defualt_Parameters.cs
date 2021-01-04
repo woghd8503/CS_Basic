9b34f12b8244e1968ad34c9bf6f719f8cda98732
DESKTOP-IAJOJ2N\user <woghd8503@naver.com>
2021-01-04 23:54:03 +0900

    // .NET 4.0 이전에는 디폴트 매개변수가 지원되지 않다가 4.0 부터 지원 되기 시작했습니다.

---
+++ ConsoleApp1/_36_Defualt_Parameters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Bitcamp
+{
+    class _36_Defualt_Parameters
+    {
+        public static void PrintMessage(string m = "thank You", int n = 1)
+        {
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine(m);
+            }
+        }
+
+        static void Main()
+        {
+            PrintMessage("Hello", 2);
+            PrintMessage("Good Morning", 3);
+            PrintMessage("Print Only once");
+            PrintMessage();
+        }
+    }
+}
+
+// .NET 4.0 이전에는 디폴트 매개변수가 지원되지 않다가 4.0 부터 지원 되기 시작했습니다.
