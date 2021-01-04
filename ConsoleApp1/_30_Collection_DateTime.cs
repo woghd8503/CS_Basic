d80fd0f577941c5699d617705f0c00d657ffc75d
DESKTOP-IAJOJ2N\user <woghd8503@naver.com>
2021-01-04 22:42:16 +0900

    // Collection_DateTime 날짜계산

---
+++ ConsoleApp1/_30_Collection_DateTime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Bitcamp
+{
+    class _30_Collection_DateTime
+    {
+        static void Main()
+        {
+            DateTime A = new DateTime(2020, 1, 4);
+            DateTime B = new DateTime(2016, 05, 31);
+            TimeSpan C = A - B; // 2개간의 시간을 측정
+            Console.WriteLine(C);
+
+            A = new DateTime(2020, 1, 4);
+            C = new TimeSpan(100, 0, 0, 0); // 100일 후
+            B = A + C;
+            Console.WriteLine(B);
+        }
+    }
+}
+
+// Collection_DateTime 날짜계산
