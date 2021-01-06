9e2ca790698ee41abc7b375af006321337dc7e14
DESKTOP-IAJOJ2N\user <woghd8503@naver.com>
2021-01-06 22:55:37 +0900

    // const 키워드를 붙인 멤버는 상수 멤버가 됩니다. // 상수 멤버의 특징은 한 번 값이 정해지면 바꿀 수 없다는 것입니다.

---
+++ ConsoleApp1/_50_Const_Member.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Bitcamp
+{
+    class _45_Const_Member
+    {
+        class SEASON
+        {
+            public const string SPRING = "봄";
+            public const string SUMMER = "여름";
+            public const string FALL = "가을";
+            public const string WINTER = "겨울";
+        }
+
+        class CSMain
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("{0}", SEASON.SPRING);
+                Console.WriteLine("{0}", SEASON.SUMMER);
+                Console.WriteLine("{0}", SEASON.FALL);
+                Console.WriteLine("{0}", SEASON.WINTER);
+            }
+        }
+    }
+}
+
+// const 키워드를 붙인 멤버는 상수 멤버가 됩니다.
+// 상수 멤버의 특징은 한 번 값이 정해지면 바꿀 수 없다는 것입니다.
+// 또 별도로 static을 붙이지 않아도 자동으로 정적 멤버가 됩니다.
+// 그러므로 객체를 생성하지 않아도 호출이 가능합니다.
---
+++ ConsoleApp1/_51_Readonly.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Bitcamp
+{
+    class _51_Readonly
+    {
+        static void Main()
+        {
+            Kingdom Korea;
+            Korea = new Kingdom("왕건", 45, 2500);
+            Korea.Intro();
+            Kingdom Josun;
+            Josun = new Kingdom("이성계", 52, 30000);
+            Josun.Intro();
+        }
+    }
+    class Human
+    {
+        private string Name;
+        private int Age;
+        public Human(string aName, int aAge)
+        {
+            Name = aName;
+            Age = aAge;
+        }
+        public void Intro()
+        {
+            Console.WriteLine("이름:" + Name);
+            Console.WriteLine("나이:" + Age);
+        }
+    }
+
+    class Kingdom
+    {
+        readonly private Human King;
+        private int Vassal;
+        public Kingdom(string aName, int aAge, int aVassal)
+        {
+            King = new Human(aName, aAge);
+            Vassal = aVassal;
+        }
+        public void Intro()
+        {
+            Console.WriteLine("왕은?");
+            King.Intro();
+            Console.WriteLine("신하:{0}명", Vassal);
+        }
+    }
+}
+
+// const는 프로그래밍 시 정해놓은 값을 변경할 수 없습니다.
+// 일단 변수는 항상 값이 변합니다.
+// 이에 객체를 생성 시에 한번 정하면 이후에는 바뀔 수 없도록 하는 중간 역할을 하는 멤버가 요구되었습니다.
+
+// 이것이 readonly 키워드를 부여한 멤버입니다.
+// 이것은 생성자에서 처음 초기화할 수 있고 이후에는 바꿀 수가 없습니다.
+// 이렇게 사용하면 const 상수에 비해 객체마다 다른 값을 부여할 수 있는 유연성과 이후로는 바뀔 수
+// 없기 때문에 에러를 방지할 수 있는 장점을 가지게 됩니다.
