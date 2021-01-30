507d9a078da987da8cb106f4eacfe105cab9798d
DESKTOP-IAJOJ2N\user <woghd8503@naver.com>
2021-01-31 01:40:52 +0900

    // Generic

---
+++ ConsoleApp1/_79_Generic_Queue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Bitcamp
+{
+    class _79_Generic_Queue
+    {
+        static void Main()
+        {
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+
+            while (queue.Count > 0)
+                Console.WriteLine(queue.Dequeue());
+        }
+    }
+}
---
+++ ConsoleApp1/_80_Generic_Stack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Bitcamp
+{
+    class _80_Generic_Stack
+    {
+        static void Main()
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            while (stack.Count > 0)
+                Console.WriteLine(stack.Pop());
+        }
+    }
+}
