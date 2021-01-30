14d9d31085e86f8205541c71fa1e34f2496f454a
DESKTOP-IAJOJ2N\user <woghd8503@naver.com>
2021-01-31 01:45:29 +0900

    // generic_dictionary

---
+++ ConsoleApp1/_81_Generic_Dictionary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Bitcamp
+{
+    class _81_Generic_Dictionary
+    {
+        static void Main()
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+
+            dic["하나"] = "one";
+            dic["둘"] = "two";
+            dic["셋"] = "three";
+            dic["넷"] = "four";
+            dic["다섯"] = "five";
+
+            Console.WriteLine(dic["하나"]);
+            Console.WriteLine(dic["둘"]);
+            Console.WriteLine(dic["셋"]);
+            Console.WriteLine(dic["넷"]);
+            Console.WriteLine(dic["다섯"]);
+        }
+    }
+}
