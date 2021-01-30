847f96ab88287bcccbf9b3d2f88251b73933dd93
DESKTOP-IAJOJ2N\user <woghd8503@naver.com>
2021-01-31 01:59:58 +0900

    // C#은 기본적으로 포인터의 접근을 권장하지 않습니다. // 하지만 Windows API난 DLL과의 연동을 위해서 포인터를 사용해야 할 수도 있습니다.

---
+++ ConsoleApp1/_82_Pointer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Bitcamp
+{
+    class _82_Pointer
+    {
+        static void Main()
+        {
+            int i = 3, j;
+            unsafe
+            {
+                int* pi;
+                pi = &i;
+                j = *pi;
+            }
+            Console.WriteLine("i={0},j={1}", i, j);
+        }
+    }
+}
+
+// C#은 기본적으로 포인터의 접근을 권장하지 않습니다.
+// 하지만 Windows API난 DLL과의 연동을 위해서 포인터를 사용해야 할 수도 있습니다.
+
+// 또, C# 언어가 기존의 C, C++을 계승하였고, C, ++로 구현된 많은 강력한 기능을 외면할
+// 수 없기에 특정 옵션을 통해서 포인터의 접근을 허용하고 있습니다.
+
+// 포인터를 사용할 수는 없으나 아무래도 에러의 위험이 많기 때문에 주의해서 사용해야 합니다.
---
+++ ConsoleApp1/_83_PointerClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Bitcamp
+{
+    class _83_PointerClass
+    {
+        static void Main()
+        {
+            int[] ar = { 1, 2, 3, 4, 5 };
+            unsafe
+            {
+                fixed (int* pi = &ar[0])
+                {
+                    for(int i = 0; i < 5; i++)
+                    {
+                        Console.WriteLine(pi[i]);
+                    }
+                }
+            }
+        }
+    }
+}
+
+// 구조체와 다르게 클래스는 Heap 메모리에 로딩됩니다. C#은 unsafe만으로 접근을 허용하지 않습니다.
+// fixed라는 영역을 추가해서 CLR에게 메모리의 이동을 금지시켜야 합니다. 
+
+// CLR은 Garbage Collector에 의한 잦은 메모리 해제로 인한 조각 메모리를 없애기 위해 잦은 메모리 이동을 하게 됩니다.
+// 하지만 위처럼 fixed는 이 영역에서의 메모리 이동을 막게 됩니다.
