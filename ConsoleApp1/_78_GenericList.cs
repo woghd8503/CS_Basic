2c03b69ee498c383d9061b187cade232ec97f209
DESKTOP-IAJOJ2N\user <woghd8503@naver.com>
2021-01-31 01:40:42 +0900

    // Generic

---
+++ ConsoleApp1/_78_GenericList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Bitcamp
+{
+    class _78_GenericList
+    {
+        static void Main()
+        {
+        List<int> list = new List<int>();
+        for(int i = 0; i < 5; i++)
+            list.Add(i);
+
+            foreach(int element in list)
+            Console.Write("{0}", element);
+            Console.WriteLine();
+            list.RemoveAt(2);
+        }
+    }
+}
