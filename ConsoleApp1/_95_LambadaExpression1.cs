69ccd6cd74f90f9da3a1f6b1cdc1cf1b664d7661
DESKTOP-IAJOJ2N\user <woghd8503@naver.com>
2021-01-31 11:40:12 +0900

    // a => a + 1; 에서 왼쪽의 a는 메서드에 전달되는 인자입니다. // 그리고 오른쪽의 a+1은 return 값입니다. // 그리고 이렇게 정의된 표현식은 메서드가 되어 delegate의 변수에 담겨서 사용되게 됩니다.

---
+++ ConsoleApp1/_95_LambadaExpression1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Bitcamp
+{
+    delegate int dele(int a);
+    class _95_LambadaExpression1
+    {
+        static void Main()
+        {
+            dele d = delegate (int a) { return a + 1; };
+            int k = d(3);
+            Console.WriteLine("k = " + k);
+        }
+    }
+}
+
+// 바로 익명 메서드라고 하는 방법입니다.
+// 하지만 보다 축약시킬 수 있는 방법을 원했습니다.
+// 이것은 메서드를 수식으로 표현하는 방식으로 람다식이라 불리우고 있습니다.
