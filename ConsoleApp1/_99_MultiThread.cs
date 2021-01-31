7105e7c47f1c676a7c3a0e3b42cb5c76e9902876
DESKTOP-IAJOJ2N\user <woghd8503@naver.com>
2021-01-31 14:59:20 +0900

    // DLL

---
+++ ConsoleApp1/_99_MultiThread.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Cs_Bitcamp
+{
+    class _99_MultiThread
+    {
+        // 작업 스레드 
+        static void ThreadProc()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(500);
+            }
+            Console.WriteLine("작업 스레드 종료");
+        }
+        // 주 스레드
+        static void Main()
+        {
+            Thread T = new Thread(new ThreadStart(ThreadProc));
+            T.Start();
+            for(; ; )
+            {
+                ConsoleKeyInfo cki;
+                cki = Console.ReadKey();
+                if(cki.Key == ConsoleKey.A)
+                {
+                    Console.Beep();
+                }
+                if (cki.Key == ConsoleKey.B)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("주 스레드 종료");
+        }
+    }
+}
