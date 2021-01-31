23cb091a4b8a51f253086280eb2b50bcf8ebd83e
DESKTOP-IAJOJ2N\user <woghd8503@naver.com>
2021-01-31 15:16:09 +0900

    // 그림판 열고 5초 뒤 닫기

---
+++ ConsoleApp1/_99_Process.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Cs_Bitcamp
+{
+    class _99_Process
+    {
+        static void Main()
+        {
+            Process Proc = Process.Start("mspaint.exe");
+            Thread.Sleep(5000);
+            Proc.Kill();
+        }
+    }
+}
