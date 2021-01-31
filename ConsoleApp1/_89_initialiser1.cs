e430a988d804c9cdf5fcecc86c7f4986cd62c41f
DESKTOP-IAJOJ2N\user <woghd8503@naver.com>
2021-01-31 10:47:02 +0900

    // 일반적으로 생성자를 통해 멤버의 초기화를 하게 됩니다. // 생성자를 사용하지 않으면 public인 경우, 위처럼 값을 부여할 수도 있습니다. // 이런 방법 외에 초기자라는 것을 C#은 제공하고 있습니다.

---
+++ ConsoleApp1/_89_initialiser1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Bitcamp
+{
+    class Human
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    class _89_initialiser1
+    {
+        static void Main()
+        {
+            Human Jang = new Human { Name = "강길산", Age = 32 };
+            Console.WriteLine("이름 : " + Jang.Name + ", 나이 : " + Jang.Age);
+        }
+    }
+}
