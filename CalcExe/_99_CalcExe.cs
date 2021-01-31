7dc0f87b1167aee4cb9cd2c9109ccdf2b401e041
DESKTOP-IAJOJ2N\user <woghd8503@naver.com>
2021-01-31 14:51:11 +0900

    using CalcLib; 로 네임스페이스를 사용하겠다고 선언합니다. 그리고는 바로 클래스와 메서드를 사용하면 됩니다.

---
+++ CalcExe/_99_CalcExe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CalcLib;
+
+namespace CalcExe
+{
+    class _99_CalcExe
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 20, num2 = 50;
+            int result = 0;
+            result = IntCalc.Add(num1, num2);
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, result);
+            result = IntCalc.Sub(num1, num2);
+            Console.WriteLine("{0} - {1} = {2}", num1, num2, result);
+            result = IntCalc.Mul(num1, num2);
+            Console.WriteLine("{0} * {1} = {2}", num1, num2, result);
+            result = IntCalc.Div(num1, num2);
+            Console.WriteLine("{0} / {1} = {2}", num1, num2, result);
+        }
+    }
+}
---
+++ CalcExe/_99_DLL_Class_Inheritance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CalcLib;
+
+namespace CalcExe
+{
+    class IntDoubleCal : IntCalc
+    {
+        public static double AddD(double a, double b) { return a + b; }
+        public static double SubD(double a, double b) { return a - b; }
+        public static double MulD(double a, double b) { return a * b; }
+        public static double DivD(double a, double b) { return a / b; }
+
+    }
+    class _99_DLL_Class_Inheritance
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 20, num2 = 50;
+            int result = 0;
+            result = IntDoubleCal.Add(num1, num2);
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, result);
+            result = IntDoubleCal.Sub(num1, num2);
+            Console.WriteLine("{0} - {1} = {2}", num1, num2, result);
+            result = IntDoubleCal.Mul(num1, num2);
+            Console.WriteLine("{0} * {1} = {2}", num1, num2, result);
+            result = IntDoubleCal.Div(num1, num2);
+            Console.WriteLine("{0} / {1} = {2}", num1, num2, result);
+            double dnum1 = 20.3, dnum2 = 50.5;
+            double dresult = 0;
+            dresult = IntDoubleCal.AddD(dnum1, dnum2);
+            Console.WriteLine("{0} + {1} = {2}", dnum1, dnum2, result);
+            dresult = IntDoubleCal.SubD(dnum1, dnum2);
+            Console.WriteLine("{0} - {1} = {2}", dnum1, dnum2, result);
+            dresult = IntDoubleCal.MulD(dnum1, dnum2);
+            Console.WriteLine("{0} * {1} = {2}", dnum1, dnum2, result);
+            dresult = IntDoubleCal.DivD(dnum1, dnum2);
+            Console.WriteLine("{0} / {1} = {2}", dnum1, dnum2, result);
+        }
+    }
+}
