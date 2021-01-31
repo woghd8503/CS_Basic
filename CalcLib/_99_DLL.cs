c1608a010802bca98475b5d6a0ad2a1745ae4f03
DESKTOP-IAJOJ2N\user <woghd8503@naver.com>
2021-01-31 14:22:29 +0900

    // DLL

---
+++ CalcLib/_99_DLL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalcLib
+{
+    public class _99_DLL
+    {
+        public static int Add(int a, int b) { return a + b; }
+        public static int Sub(int a, int b) { return a - b; }
+        public static int Mul(int a, int b) { return a * b; }
+        public static int Div(int a, int b) { return a / b; }
+    }
+}
